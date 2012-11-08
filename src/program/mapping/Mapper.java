63dbc33c832cbfa30e0ae67a36dbd2255c000425
Michael Stevens <mls0.5mv2@gmail.com>
2012-11-08 17:02:30 -0500

    started mapping code

---
+++ src/program/mapping/Mapper.java
@@ -0,0 +1,22 @@
+package program.mapping;
+
+import java.util.ArrayList;
+
+public class Mapper {
+
+	ArrayList<Road> path = new ArrayList<Road>();
+	int pathIndex = 0;
+	
+	public Mapper() {
+		//Initialize Roads here!!
+	}
+	
+	public void findPath(Road current, Road dest) {  //Runs the A* search, puts result in path
+		
+	}
+	
+	public Road getNextRoad() { //returns the next road, Road will include directions to the next.
+		//pathIndex++;
+		return path.get(pathIndex++); //MIGHT WORK
+	}
+}
---
+++ src/program/mapping/Road.java
@@ -0,0 +1,42 @@
+package program.mapping;
+
+public class Road {
+
+	static final int RIGHT=0, LEFT=1, STRAIGHT=0;
+	
+	Road rightChild=null, leftChild=null, straightChild=null;
+	
+	int length;
+	int pDirection;
+	boolean park=false; //Is it a parking space??
+	boolean circle=false;
+	
+	public Road(int pDir) { //use this constructor if it is a parking area
+		pDirection=pDir;
+		park=true; //We assume if it has no children, it is a parking space. 
+	}
+	public Road(int l, int dir, Road right, Road left, Road straight) { //use this one for a normal road
+		length=l; 
+		pDirection=dir; 
+		rightChild=right; 
+		leftChild=left; 
+		straightChild=straight;
+	}
+	
+	public void setCircle(boolean c) { //Make it a circle
+		circle=c;
+	}
+	public boolean isCircle() { //Is it a circle???
+		return circle;
+	}
+	
+	public Road getChildRight() {
+		return rightChild;
+	}
+	public Road getChildLeft() {
+		return leftChild;
+	}
+	public Road getChildStraight() {
+		return straightChild;
+	}
+}
