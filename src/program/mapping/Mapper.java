a6c2dde843b2f7230cc9d6789555a2a4a457b498
Michael Stevens <mls0.5mv2@gmail.com>
2012-11-09 17:32:06 -0500

    formatting code

--- src/program/mapping/Mapper.java
+++ src/program/mapping/Mapper.java
@@ -6,17 +6,19 @@ public class Mapper {
 
 	ArrayList<Road> path = new ArrayList<Road>();
 	int pathIndex = 0;
-	
+
 	public Mapper() {
-		//Initialize Roads here!!
+		// Initialize Roads here!!
 	}
-	
-	public void findPath(Road current, Road dest) {  //Runs the A* search, puts result in path
-		
+
+	public void findPath(Road current, Road dest) { // Runs the A* search, puts
+													// result in path
+
 	}
-	
-	public Road getNextRoad() { //returns the next road, Road will include directions to the next.
-		//pathIndex++;
-		return path.get(pathIndex++); //MIGHT WORK
+
+	public Road getNextRoad() { // returns the next road, Road will include
+								// directions to the next.
+		// pathIndex++;
+		return path.get(pathIndex++); // MIGHT WORK
 	}
 }
