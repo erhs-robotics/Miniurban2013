5961280f7c40401e9d31f8ace6d0180a064d1618
Michael Stevens <mls0.5mv2@gmail.com>
2012-11-14 21:37:05 -0500

    outlining A star algorithm

--- src/program/mapping/Mapper.java
+++ src/program/mapping/Mapper.java
@@ -4,9 +4,7 @@ import java.util.ArrayList;
 
 public class Mapper {
 
-	ArrayList<Road> path = new ArrayList<Road>();
-	ArrayList<Road> open = new ArrayList<Road>(); // The nodes that need to be expanded
-	ArrayList<Road> closed = new ArrayList<Road>(); // The nodes that have already been expanded
+	ArrayList<Road> path = new ArrayList<Road>();	
 	
 	int pathIndex = 0;
 
@@ -25,16 +23,27 @@ public class Mapper {
 		left.setRightChild(top);
 	}
 
-	public void findPath(Road current, Road dest) { // Runs the A* search, puts
-													// result in path
+	public void findPath(Road current, Road goal) { // Runs the A* search, puts result in path
+		ArrayList<Road> open = new ArrayList<Road>(); // The nodes that need to be expanded
+		ArrayList<Road> closed = new ArrayList<Road>(); // The nodes that have already been expanded
+		boolean goalhit = false;
 		
-		
+		while(!goalhit) {
+			goalhit = expand(current, open, closed);// Expand from the current node to the surrounding nodes
+			if(!goalhit) current = getBestNode(open);// Picks the next current node that will most likely lead to the goal		
+		}
 
 	}
 	
-	private void expand() {		
+	// returns the node in open with the smallest gvalue + h(x) where h(x) is the heuristic
+	public Road getBestNode(ArrayList<Road> open) { 
+		return null;
+	}
+	
+	//expands open into the nodes surrounding current and checks if we've hit the goal
+	private boolean expand(Road current, ArrayList<Road> open, ArrayList<Road> closed) {		
 		
-		
+		return false;
 	}
 
 	public Road getNextRoad() { // returns the next road, Road will include
