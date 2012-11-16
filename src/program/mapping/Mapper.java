7f5146e708b39459d2e739226a028e5f79c8658d
Michael Stevens <mls0.5mv2@gmail.com>
2012-11-15 20:39:18 -0500

    added expansion algorithm

--- src/program/mapping/Mapper.java
+++ src/program/mapping/Mapper.java
@@ -29,7 +29,7 @@ public class Mapper {
 		boolean goalhit = false;
 		
 		while(!goalhit) {
-			goalhit = expand(current, open, closed);// Expand from the current node to the surrounding nodes
+			goalhit = expand(current, open, closed, goal);// Expand from the current node to the surrounding nodes
 			if(!goalhit) current = getBestNode(open);// Picks the next current node that will most likely lead to the goal		
 		}
 
@@ -41,9 +41,36 @@ public class Mapper {
 	}
 	
 	//expands open into the nodes surrounding current and checks if we've hit the goal
-	private boolean expand(Road current, ArrayList<Road> open, ArrayList<Road> closed) {		
+	private boolean expand(Road current, ArrayList<Road> open, ArrayList<Road> closed, Road goal) {		
+		//getChild returns null if no child
+		Road right    = current.getChildRight();
+		Road left     = current.getChildLeft();
+		Road straight = current.getChildStraight();
 		
-		return false;
+		//if child exists and we have not expanded it yet
+		if(right != null && !closed.contains(right)) {			
+			right.setG_value(current.getG_value() + right.getLength());//record the cost of getting here
+			if(right.getName() == goal.getName()) return true;//check if we made it to the goal
+			open.add(right);//add road to the open list so it can be expanded in the future
+		}
+		
+		if(left != null && !closed.contains(left)) {			
+			left.setG_value(current.getG_value() + left.getLength());
+			if(left.getName() == goal.getName()) return true;
+			open.add(left);
+		}
+		
+		if(straight != null && !closed.contains(straight)) {			
+			straight.setG_value(current.getG_value() + straight.getLength());
+			if(straight.getName() == goal.getName()) return true;
+			open.add(straight);
+		}
+		
+		closed.add(current);//make sure we don't expand current again
+		
+		
+		
+		return false;//false because we did not find the goal yet
 	}
 
 	public Road getNextRoad() { // returns the next road, Road will include
