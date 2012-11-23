ae4eda17d095d6a4fade92c7d83f2874c4a4100c
Michael Stevens <mls0.5mv2@gmail.com>
2012-11-22 22:02:16 -0500

    adding assertions

--- src/program/mapping/Mapper.java
+++ src/program/mapping/Mapper.java
@@ -2,6 +2,8 @@ package program.mapping;
 
 import java.util.ArrayList;
 
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
 public class Mapper {
 
 	public Mapper() {
@@ -9,6 +11,7 @@ public class Mapper {
 	}
 	
 	public ArrayList<Step> getPath(Road current, ArrayList<Goal> goals) throws Exception {
+		if(goals.size() < 1) throw new Exception("No goals given! Must have at least one goal!");
 		//Store were we started so we can return here at the end
 		Goal start = new Goal(current.getName(), 0, Direction.None);
 		//store start in a convenient list so we can pass it to findPath()
@@ -34,14 +37,19 @@ public class Mapper {
 	}
 
 	private ArrayList<Step> findPath(Road current, ArrayList<Goal> goals) throws Exception { // Runs the A* search, puts result in path
+		assert goals.size() > 0;
+		assert current != null;
+		assert current.hasLeftChild() || current.hasRightChild() || current.hasStraightChild();
+		
 		ArrayList<Road> open = new ArrayList<Road>(); // The nodes that need to be expanded
 		ArrayList<Road> closed = new ArrayList<Road>(); // The nodes that have already been expanded
 		Road goal = null;
 		current.setG_value(0);// make sure we start at 0
+		open.add(current);//to comply with expand assertions
 		while(goal == null) {// while we have not hit a goal
 			goal = expand(current, open, closed, goals);// Expand from the current node to the surrounding nodes
 			if(goal == null) current = getBestNode(open);// Picks the next current node that will most likely lead to the goal		
-		}		
+		}
 		
 		//travels backward form goal to the start and records that path
 		//also removes the goals we just found from goals
@@ -55,13 +63,21 @@ public class Mapper {
 				path.add(nextPath.get(i));
 			}
 		}
+		assert path.size() > 0;
+		assert goals.size() == 0;
 		//if goals.size() = 0, we are done so return the full path
 		return path;
 	}
 	//takes the expanded map and works backward from goal to the start and generates the directions
-	public ArrayList<Step> genPath(Road goal, ArrayList<Goal> goals) throws Exception {		
+	private ArrayList<Step> genPath(Road goal, ArrayList<Goal> goals) throws Exception {
+		assert goal != null;
+		assert goals.size() > 0;
+		assert goal.hasExpandedLeftParent() || goal.hasExpandedRightParent() || goal.hasExpandedStraightParent();
+		assert goal.getG_value() > 0;
+		
 		ArrayList<Step> path = new ArrayList<Step>();
 		Goal goalInfo = Goal.getGoal(goals, goal);
+		assert goalInfo != null;
 		//give parking information
 		path.add(new Park(goal, goalInfo.getDirection(), goalInfo.getPark()));
 		
@@ -80,23 +96,28 @@ public class Mapper {
 			if(current.hasExpandedLeftParent()) parents.add(left);
 			if(current.hasExpandedStraightParent()) parents.add(straight);
 			
-			if(parents.size() < 1) throw new Exception("Node has no parents!");//all parents are null
+			assert parents.size() > 0;//all parents are null
 			
 			Road best = getBestNode(parents);//find the node with the least g_value that is not -1
 			
 			//record in path, reverses directions because this function travels backwards: from goal to start
 			if(best == right) path.add(0, new Turn(right, Direction.Left));
-			if(best == left) path.add(0, new Turn(left, Direction.Right));
-			if(best == straight) path.add(0, new Turn(straight, Direction.Straight));
+			else if(best == left) path.add(0, new Turn(left, Direction.Right));
+			else if(best == straight) path.add(0, new Turn(straight, Direction.Straight));
+			else assert false;//code should not reach this!
 			
 			current = best;// record best in current so we can continue traveling backwards toward the start
 			
 		}
+		assert current.getG_value() == 0;
+		assert path.size() > 0;
+		assert path.get(path.size() - 1) instanceof Park;		
 		return path;
 	}
 	
 	// returns the node in open with the smallest gvalue + h(x) where h(x) is the heuristic
-	public Road getBestNode(ArrayList<Road> open) { 
+	private Road getBestNode(ArrayList<Road> open) {
+		assert open.size() > 0;		
 		Road best = open.get(0);
 		for(int i=0;i<open.size();i++) {
 			Road road = open.get(i);			
@@ -104,11 +125,20 @@ public class Mapper {
 				best = road;
 			}
 		}
+		assert best.getG_value() != -1;
+		assert best != null;
 		return best;		
 	}
 	
 	//expands open into the nodes surrounding current. Returns the first goal hit, if one is hit and returns null otherwise
-	private Road expand(Road current, ArrayList<Road> open, ArrayList<Road> closed, ArrayList<Goal> goals) {		
+	private Road expand(Road current, ArrayList<Road> open, ArrayList<Road> closed, ArrayList<Goal> goals) {
+		assert current != null;
+		assert goals.size() > 0;
+		assert !closed.contains(current);
+		assert open.contains(current);
+		assert current.hasLeftChild() || current.hasRightChild() || current.hasStraightChild();
+		assert !Goal.isGoal(goals, current);
+		
 		//getChild returns null if no child
 		Road right    = current.getChildRight();
 		Road left     = current.getChildLeft();
@@ -118,27 +148,36 @@ public class Mapper {
 		//if child exists and we have not expanded it yet
 		if(current.hasRightChild() && !closed.contains(right)) {			
 			right.setG_value(current.getG_value() + right.getCost());//record the cost of getting here
-			if(Goal.isGoal(goals, right)) return right; //check if we hit one of the goals, if so return it
+			if(Goal.isGoal(goals, right)) {
+				assert !closed.contains(right);
+				return right; //check if we hit one of the goals, if so return it
+			}
 			
 			open.add(right);//add road to the open list so it can be expanded in the future
 		}
 		
 		if(current.hasLeftChild() && !closed.contains(left)) {			
 			left.setG_value(current.getG_value() + left.getCost());
-			if(Goal.isGoal(goals, left)) return left;
+			if(Goal.isGoal(goals, left)){
+				assert !closed.contains(left);
+				return left;
+			}
 			open.add(left);
 		}
 		
 		if(current.hasStraightChild() && !closed.contains(straight)) {			
 			straight.setG_value(current.getG_value() + straight.getCost());
-			if(Goal.isGoal(goals, straight)) return straight;
+			if(Goal.isGoal(goals, straight)){
+				assert !closed.contains(straight);
+				return straight;
+			}
 			open.add(straight);
 		}
 		
 		closed.add(current);//make sure we don't expand current again
 		open.remove(current);//remove from open because it has already been expanded
 		
-		
+		assert open.size() > 0;
 		return null;//null because we did not find the goal yet
 	}	
 }
