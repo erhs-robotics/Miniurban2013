ad6de23eee2b4a9a83bd8a420b19fa1e269b343e
Michael Stevens <mls0.5mv2@gmail.com>
2013-03-07 17:11:11 -0500

    fixing string compare

--- src/program/main/Main.java
+++ src/program/main/Main.java
@@ -20,12 +20,12 @@ public class Main {
 		ArrayList<Goal> goals = new ArrayList<Goal>();
 		
 		goals.add(new Goal("R1", 2, Direction.Left));
-		//goals.add(new Goal("R15", 4, Direction.Right));
-		//goals.add(new Goal("R45", 4, Direction.Right));
-		//goals.add(new Goal("R55", 4, Direction.Right));
-		//goals.add(new Goal("R69", 4, Direction.Right));
-		//goals.add(new Goal("R36", 4, Direction.Right));
-		//goals.add(new Goal("start", 0, Direction.Left));
+		goals.add(new Goal("R15", 4, Direction.Right));
+		goals.add(new Goal("R45", 4, Direction.Right));
+		goals.add(new Goal("R55", 4, Direction.Right));
+		goals.add(new Goal("R69", 4, Direction.Right));
+		goals.add(new Goal("R36", 4, Direction.Right));
+		
 		
 		Mapper mapper = new Mapper();
 		ArrayList<Step> path;
