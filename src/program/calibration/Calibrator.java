9df280b1dd30d0fbee2d34c424dcfa839d6ee67c
Nick Yaculak <nyaculak@gmail.com>
2013-03-20 19:54:54 -0400

    printing out results of color calibration

--- src/program/calibration/Calibrator.java
+++ src/program/calibration/Calibrator.java
@@ -1,9 +1,12 @@
 package program.calibration;
 
+import program.main.MathUtils;
 import lejos.nxt.Button;
 import lejos.nxt.addon.ColorHTSensor;
 
 public class Calibrator {
+	
+	public static final int VARIANCE = 30;
 	
 	public static void calibrateColor(ColorHTSensor sensor) {
 		
@@ -30,9 +33,25 @@ public class Calibrator {
 			blue[i]   = sensor.getRGBComponent(ColorHTSensor.BLUE);
 			yellow[i] = sensor.getRGBComponent(ColorHTSensor.YELLOW);
 		}
+				
+		System.out.println("BLACK : " + min(black) + "/" + max(black));
+		System.out.println("WHITE : " + min(white) + "/" + max(white));
+		System.out.println("RED   : " + min(red)   + "/" + max(red));
+		System.out.println("GREEN : " + min(green) + "/" + max(green));
+		System.out.println("BLUE  : " + min(blue)  + "/" + max(blue));
+		System.out.println("YELLOW: " + min(yellow) + "/" + max(yellow));
 		
-		// ToDo: Print out results of calibration
-		
+	}
+	
+	private static String min(int[] array) {
+		int out = MathUtils.arrayMin(array) - VARIANCE;
+		if (out < 0) out = 0;
+		return Integer.toString(out);
+	}
+	private static String max(int[] array) {
+		int out = MathUtils.arrayMax(array) - VARIANCE;
+		if (out > 255) out = 255;
+		return Integer.toString(out);
 	}
 	
 }
