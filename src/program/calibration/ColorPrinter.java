84d0b0b62b44143ce4e5303a4431d946321a62e9
Nick Yaculak <nyaculak@gmail.com>
2013-03-22 16:17:50 -0400

    adding ColorPrinter class with main function

---
+++ src/program/calibration/ColorPrinter.java
@@ -0,0 +1,28 @@
+package program.calibration;
+
+import lejos.nxt.addon.ColorHTSensor;
+import lejos.nxt.comm.RConsole;
+import program.main.Robot;
+
+public class ColorPrinter {
+	
+	public static void main(String args[]) {
+		
+		Robot robot = new Robot();
+		
+		while(true) {
+			String yellow = String.valueOf(robot.leftColorSensor.getRGBComponent(ColorHTSensor.YELLOW));
+			String red = String.valueOf(robot.leftColorSensor.getRGBComponent(ColorHTSensor.RED));
+			String black = String.valueOf(robot.leftColorSensor.getRGBComponent(ColorHTSensor.BLACK));
+			String white = String.valueOf(robot.leftColorSensor.getRGBComponent(ColorHTSensor.WHITE));
+			String blue = String.valueOf(robot.leftColorSensor.getRGBComponent(ColorHTSensor.BLUE));
+			String green = String.valueOf(robot.leftColorSensor.getRGBComponent(ColorHTSensor.GREEN));
+			String out = "R: " + red + ", G: " + green + ", B: " + blue + ", Y: " + yellow + ", W: " + white + ", Black: " + black;
+		    String color = robot.checkColor(robot.leftColorSensor);
+		    out += ", Color: " + color;
+			RConsole.println(out);
+		}
+		
+	}
+
+}
